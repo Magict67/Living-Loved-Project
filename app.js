6b8fc86ddeb92ea505475094fb670ca033fc8103
magict67 <christopheradonaithomas@gmail.com>
2026-01-31 18:31:12 -0800

    run nodeappjs for srvr chk

--- app.js
+++ app.js
@@ -3,12 +3,10 @@ const fs = require('fs');
 const app = express();
 const port = 3000;
 
-// server allow view in'public' folder
 app.use(express.static('public'));
 
 const dataPath = './data/messages.json';
 
-// Route for random message (bk end)
 app.get('/api/daily-message', (req, res) => {
     fs.readFile(dataPath, 'utf8', (err, data) => {
         if (err) {

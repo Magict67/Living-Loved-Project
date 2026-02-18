3a533a00ce80ff710a42ba372d2993cb88e969da
magict67 <christopheradonaithomas@gmail.com>
2026-02-17 17:08:35 -0800

    Designate port spec.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const session = require('express-session');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.static('public'));
